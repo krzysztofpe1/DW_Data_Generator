b7ad459a9affb3cb792624001fca8aa867bbb802
Krzysztof Pecyna <krzysztofpe-01@o2.pl>
2023-11-04 16:40:07 +0100

    Solution creation

---
+++ DW_Data_Generator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DW_Data_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
