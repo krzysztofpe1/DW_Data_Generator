dbe5ffc676b7e57d0e2c6706a9d77858cda9d360
Krzysztof Pecyna <krzysztofpe-01@o2.pl>
2023-11-04 19:45:35 +0100

    Added DataGenerator class.

---
+++ DW_Data_Generator/DataGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DW_Data_Generator
+{
+    public class DataGenerator
+    {
+        public DateTime StartDate { get; set; }
+        public int RepairsPerDay { get; set; }
+        public int RepairsPerDayOscilation { get; set; }
+        public DateTime T1 { get; set; }
+        public DateTime T2 { get; set; }
+        public int LimitRecords { get; set; }
+        public int MechanicCount { get; set; }
+        public int RegularClients { get; set; }
+        public double ChanceForNewClient { get; set; }
+        public DataGenerator() { }
+
+        public void GenerateData()
+        {
+
+        }
+    }
+}
